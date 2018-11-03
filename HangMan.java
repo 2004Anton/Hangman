656e0629d92a54699810d63fcd4a955f05d1fab8
2004Anton <44718582+2004Anton@users.noreply.github.com>
2018-11-02 22:04:04 -0700

    Add files via upload

--- HangMan.java
+++ HangMan.java
@@ -1,14 +1,15 @@
 public class HangMan{
-    private int tries;
-    private String word;
-    private String revealed;
-    private String guessed;
+    private int tries; //number of mis-guesses
+    private String word; //word that they want to guess
+    private String revealed; //how much of the word they have guessed
+    private String guessed; //what letters they have guessed
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public HangMan(String word,int tries){
         this.word = word;
         this.tries = tries;
         this.revealed = "";
         this.guessed = "";
+        //To 
         for(int i = 0; i < word.length(); i++){
         	if(ALPHABET.contains(word.substring(i,i+1).toLowerCase())) {
         		revealed = revealed + "_";
@@ -22,27 +23,28 @@ public class HangMan{
         }
     }
     public void update(){
-        System.out.println("Number of mis-guesses left: " + tries);
+    	System.out.println("The Length Of The Phrase Is: " + word.length() + " (includes non-characters)");
+        System.out.println("Number Of Missed Guesses Left: " + tries);
         System.out.println("What You Have Revealed So Far: " + revealed);
         System.out.println("Letters You Have Guessed: " + guessed);
     }
     public int isLetter(char guess){
         for(int i = 0; i < guessed.length();i++){
             if (guessed.charAt(i) == guess){
-                return 0;
+                return 0; // Has already been guessed
             }
         }
         for(int i = 0; i < ALPHABET.length(); i++) {
         	if(ALPHABET.charAt(i) == guess) {
         		 for(int k = 0; k < word.length(); k++){
         	         if(word.toLowerCase().charAt(k) == guess){
-        	            return 2;
+        	            return 3;//in the alphabet and in the word
         	         }
         		 }
-        		 return 1;
+        		 return 2;//in the alphabet, but not in the word
         	}
         }
-       return 0;
+       return 1; //not in the alphabet, so they cannot guess it
         
     }
     public void updateLetter(char guess){
--- Main.java
+++ Main.java
@@ -4,39 +4,54 @@ public class Main {
     	boolean playAgain = true;
     	while(playAgain) {
 	        Scanner s = new Scanner(System.in);
+	        boolean validWord = false;
+	        String word = "";
 	        System.out.println("What is the word or phrase you want your friend to guess?");
-	        String word = s.nextLine();
-	        System.out.println("How many mis-guesses do you want to give them?");
+	        while (validWord == false) {
+	        	word = s.nextLine();
+		        if(!(word.contains("_"))) {
+		        	validWord = true;
+		        }
+		        else {
+		        	System.out.println("That was an invalid word. Please input another");
+		        }
+	        }
+	        System.out.println("How many missed guesses do you want to give them?");
 	        int tries = s.nextInt();
-	        HangMan p = new HangMan(word,tries);
+	        HangMan game = new HangMan(word,tries);
 	        for(int i = 0; i < 100; i++) {
 	        	System.out.println();
 	        }
-	        while(p.winCondition() == 1){
-	        	p.update();
+	        while(game.winCondition() == 1){
+	        	game.update();
 	        	System.out.println("Enter your guess here: ");
 	            char g = s.next().toLowerCase().charAt(0);
-	            if(p.isLetter(g) == 0){
+	            if(game.isLetter(g) == 0){
 	                System.out.println("You Have Already Guessed The Letter: " + g + ". Guess Again");
 	            }
-	            else if(p.isLetter(g) == 2){
+	            else if(game.isLetter(g) == 1) {
+	            	System.out.println("You Cannot Guess That Character!");
+	            }
+	            else if(game.isLetter(g) == 3){
 	                System.out.println("This Letter Was In The Word: "+ g);
-		            p.updateLetter(g);
+		            game.updateLetter(g);
 	            }
 	            else{
 	                System.out.println("Ouch! The Letter You Guessed Was Not In The Word: " + g);
-	                p.subtractTries();
-		            p.updateLetter(g);
+	                game.subtractTries();
+		            game.updateLetter(g);
 	            }
 	        }
-	        p.update();
-	        if(p.winCondition() == 2) {
+	        game.update();
+	        if(game.winCondition() == 2) {
 	        	System.out.println("YOU WIN");
 	        }
-	        if(p.winCondition() == 0) {
+	        if(game.winCondition() == 0) {
 	        	System.out.println("YOU LOSE");
 	        }
 	        System.out.println("Do You Want To Play Again? y/n");
+	        s.nextLine();
+	        
 	        String answer = s.nextLine();
 	        if(answer.toLowerCase().equals("y")) {
 	        	playAgain = true;
