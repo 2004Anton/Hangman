4e57116151407a075bfd1e55ed399ad9c0e6cbcd
2004Anton <44718582+2004Anton@users.noreply.github.com>
2018-11-02 21:22:00 -0700

    Add files via upload

---
+++ HangMan.java
@@ -0,0 +1,75 @@
+public class HangMan{
+    private int tries;
+    private String word;
+    private String revealed;
+    private String guessed;
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public HangMan(String word,int tries){
+        this.word = word;
+        this.tries = tries;
+        this.revealed = "";
+        this.guessed = "";
+        for(int i = 0; i < word.length(); i++){
+        	if(ALPHABET.contains(word.substring(i,i+1).toLowerCase())) {
+        		revealed = revealed + "_";
+        	}
+        	else {
+        		revealed = revealed + word.substring(i,i+1);
+        	}
+        }
+        for(int i = 0; i < ALPHABET.length();i++) {
+        	guessed = guessed + " ";
+        }
+    }
+    public void update(){
+        System.out.println("Number of guesses left: " + tries);
+        System.out.println("What You Have Revealed So Far: " + revealed);
+        System.out.println("Letters You Have Guessed: " + guessed);
+    }
+    public int isLetter(char guess){
+        for(int i = 0; i < guessed.length();i++){
+            if (guessed.charAt(i) == guess){
+                return 0;
+            }
+        }
+        for(int i = 0; i < ALPHABET.length(); i++) {
+        	if(ALPHABET.charAt(i) == guess) {
+        		 for(int k = 0; k < word.length(); k++){
+        	         if(word.toLowerCase().charAt(k) == guess){
+        	            return 2;
+        	         }
+        		 }
+        		 return 1;
+        	}
+        }
+       return 0;
+        
+    }
+    public void updateLetter(char guess){
+        for (int i = 0; i < word.length(); i++){
+            if(word.toLowerCase().charAt(i) == guess){
+                revealed = revealed.substring(0,i) + guess + revealed.substring(i+1,revealed.length());
+            }
+        }
+        for(int i = 0; i < ALPHABET.length();i++) {
+        	if(ALPHABET.charAt(i) == guess) {
+        		guessed = guessed.substring(0,i) + guess + guessed.substring(i+1,guessed.length());
+        	}
+        }
+    }
+    public void subtractTries() {
+    	tries--;
+    }
+    public int winCondition(){
+        if (tries <= 0){
+            return 0;
+        }
+        for(int i = 0; i < revealed.length();i++){
+            if(revealed.charAt(i) == '_'){
+                return 1;
+            }
+        }
+        return 2;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+    	boolean playAgain = true;
+    	while(playAgain) {
+	        Scanner s = new Scanner(System.in);
+	        System.out.println("What is the word or phrase you want your friend to guess?");
+	        String word = s.nextLine();
+	        System.out.println("How many tries do you want to give them?");
+	        int tries = s.nextInt();
+	        HangMan p = new HangMan(word,tries);
+	        for(int i = 0; i < 100; i++) {
+	        	System.out.println();
+	        }
+	        while(p.winCondition() == 1){
+	        	System.out.println("Enter your guess here: ");
+	            char g = s.next().toLowerCase().charAt(0);
+	            if(p.isLetter(g) == 0){
+	                System.out.println("You Have Already Guessed The Letter: " + g + ". Guess Again");
+	            }
+	            else if(p.isLetter(g) == 2){
+	                System.out.println("This Letter Was In The Word: "+ g);
+		            p.updateLetter(g);
+	            }
+	            else{
+	                System.out.println("Ouch! The Letter You Guessed Was Not In The Word: " + g);
+	                p.subtractTries();
+		            p.updateLetter(g);
+	            }
+	            p.update();
+	        }
+	        if(p.winCondition() == 2) {
+	        	System.out.println("YOU WIN");
+	        }
+	        if(p.winCondition() == 0) {
+	        	System.out.println("YOU LOSE");
+	        }
+	        System.out.println("Do You Want To Play Again? y/n");
+	        String answer = s.nextLine();
+	        if(answer.toLowerCase().equals("y")) {
+	        	playAgain = true;
+	        }
+	        else {
+	        	playAgain = false;
+	        }
+    	}
+     }
+}
